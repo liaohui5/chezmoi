aa7e9ff047da0acf9c159dad61073e575712f9e6
secret <secretx500@qq.com>
2026-07-01 13:38:52 +0800

    refactor: Limit water reminders to 8 AM to 11 PM

--- dot_zsh-scripts/notify.zsh
+++ dot_zsh-scripts/notify.zsh
@@ -1,4 +1,11 @@
 #!/usr/bin/env zsh
 
-# 使用 crontab 定时提醒喝水
-osascript -e 'display dialog "到时间该喝水了" buttons {"OK"} default button "OK"'
+# 使用 crontab 定时提醒喝水: 仅 8:00-23:00 点执行
+function drink_water_notify() {
+  Hour=$(date +%H)
+  if [ "$Hour" -ge 8 ] && [ "$Hour" -lt 23 ]; then
+    osascript -e 'display dialog "到时间该喝水了" buttons {"OK"} default button "OK"'
+  fi
+}
+
+drink_water_notify
